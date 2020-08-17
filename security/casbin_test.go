153ee5f327694ac2f3c6220a155bb91a1156f4d8
llh <forever2y_y@163.com>
2020-08-17 17:28:37 +0800

    改进测试方法

--- security/casbin_test.go
+++ security/casbin_test.go
@@ -7,6 +7,13 @@ import (
 	"github.com/casbin/casbin/v2"
 )
 
+// 模型结构体。临时用。
+type modelTemp struct {
+	Sub, // the user that wants to access a resource.
+	Obj, // the resource that is going to be accessed.
+	Act string // the operation that the user performs on the resource.
+}
+
 var enforce *casbin.Enforcer
 
 func init() {
@@ -17,19 +24,35 @@ func init() {
 		fmt.Println(err)
 	}
 }
-
-func TestReadConf(t *testing.T) {
-	sub := "alice" // the user that wants to access a resource.
-	obj := "data1" // the resource that is going to be accessed.
-	act := "read"  // the operation that the user performs on the resource.
-
-	ok, err := enforce.Enforce(sub, obj, act)
+func checkPerms(model modelTemp) {
+	ok, err := enforce.Enforce(model.Sub, model.Obj, model.Act)
 	if err != nil {
-		t.Log(err)
+		fmt.Println(err)
 	}
 	if ok == true {
-		t.Logf("%s has perm.", sub)
+		fmt.Printf("%s has perm. \n", model.Sub)
 	} else {
-		t.Logf("%s has not perm.", sub)
+		fmt.Printf("%s has not perm. \n", model.Sub)
 	}
+}
+func TestReadConf(t *testing.T) {
+
+	m1 := modelTemp{
+		Sub: "alice",
+		Obj: "data1",
+		Act: "read",
+	}
+	m2 := modelTemp{
+		Sub: "Tom",
+		Obj: "data1",
+		Act: "read",
+	}
+	m3 := modelTemp{
+		Sub: "bob",
+		Obj: "data2",
+		Act: "write",
+	}
+	checkPerms(m1)
+	checkPerms(m2)
+	checkPerms(m3)
 }
